d6f998e3103b870399021002adf663fde4f51354
Feko <fekomedeiros@gmail.com>
2022-12-12 17:31:05 +0000

    Day12 - Refactor part 2, changing approach for better performance

--- AdventOfCode2022/Advent-Of-Code-2022/Day12.cs
+++ AdventOfCode2022/Advent-Of-Code-2022/Day12.cs
@@ -10,14 +10,14 @@ namespace TestProject1
 {
     public class Day12
     {
-        private int GetCost(char c) => c switch
+        public int GetCost(char c) => c switch
         {
             'S' => 'a' - 96,
             'E' => 'z' - 96,
             _ => c - 96
         };
 
-        private string GetNodeName(int line, int column) => $"N{line}x{column}";
+        private string GetNodeName(int line, int column) => $"L{line}xC{column}";
 
         public class PathContext
         {
@@ -26,7 +26,7 @@ namespace TestProject1
             public Dictionary<string, Dictionary<string, int>> Graph { get; set; }
         }
 
-        public PathContext GetGraph(string[] lines)
+        public PathContext GetGraph(string[] lines, Func<int, int, bool> comparer)
         {
             string start = "", finish = "";
 
@@ -41,25 +41,41 @@ namespace TestProject1
                     if (lines[line][column] == 'E')
                         finish = GetNodeName(line, column);
 
-                    Dictionary<string, int> neighbours = new();
-                    int thisCost = GetCost(lines[line][column]);
-
-                    if (line > 0 && GetCost(lines[line - 1][column]) <= thisCost + 1)
-                        neighbours.Add(GetNodeName(line - 1, column), GetCost(lines[line - 1][column]));
-
-                    if (line < lines.Length - 1 && GetCost(lines[line + 1][column]) <= thisCost + 1)
-                        neighbours.Add(GetNodeName(line + 1, column), GetCost(lines[line + 1][column]));
-
-                    if (column > 0 && GetCost(lines[line][column - 1]) <= thisCost + 1)
-                        neighbours.Add(GetNodeName(line, column - 1), GetCost(lines[line][column - 1]));
-
-                    if (column < lines[line].Length - 1 && GetCost(lines[line][column + 1]) <= thisCost + 1)
-                        neighbours.Add(GetNodeName(line, column + 1), GetCost(lines[line][column + 1]));
+                    var neighbours = GetNeighbours(lines, comparer, line, column);
 
                     graph.Add(GetNodeName(line, column), neighbours);
                 }
             }
             return new PathContext() { Start = start, Finish = finish, Graph = graph };
+        }
+
+        private Dictionary<string, int> GetNeighbours(string[] lines, Func<int, int, bool> comparer, int line, int column)
+        {
+            Dictionary<string, int> neighbours = new();
+            int thisCost = GetCost(lines[line][column]);
+
+            List<(int line, int column)> neighboursPosition = new()
+            {
+                (line -1, column),
+                (line +1, column),
+                (line, column -1),
+                (line, column +1),
+            };
+
+            int GetPositionCost((int line, int column) pos) => GetCost(lines[pos.line][pos.column]);
+            bool Inbounds((int line, int column) pos)
+            {
+                return pos.line >= 0 && pos.line < lines.Length &&
+                    pos.column >= 0 && pos.column < lines[line].Length;
+            }
+
+            foreach (var position in neighboursPosition)
+            {
+                if (Inbounds(position) && comparer(GetPositionCost(position), thisCost))
+                    neighbours.Add(GetNodeName(position.line, position.column), GetPositionCost(position));
+            }
+
+            return neighbours;
         }
 
         [Fact]
@@ -68,7 +84,7 @@ namespace TestProject1
             var lines = File.ReadAllLines("Inputs/day12.txt"); int expected = 462;
             //var lines = File.ReadAllLines("Inputs/day12_sample.txt"); int expected = 31;
 
-            var context = GetGraph(lines);
+            var context = GetGraph(lines, (next, thisCost) =>  next <= thisCost + 1);
 
             var costMatrix = KindOfDijikistra(context.Graph, context.Start);
 
@@ -78,23 +94,21 @@ namespace TestProject1
         [Fact]
         public void Day12_Part2()
         {
-            //var lines = File.ReadAllLines("Inputs/day12.txt"); int expected = 451;
-            var lines = File.ReadAllLines("Inputs/day12_sample.txt"); int expected = 29;
+            var lines = File.ReadAllLines("Inputs/day12.txt"); int expected = 451;
+            //var lines = File.ReadAllLines("Inputs/day12_sample.txt"); int expected = 29;
 
-            var context = GetGraph(lines);
-
+            var context = GetGraph(lines, (next, thisCost) => next >= thisCost -1);
             int lowest = Int32.MaxValue;
+            var costMatrix = KindOfDijikistra(context.Graph, context.Finish);
 
-            // TODO: This is going to be SUPER SLOW, try to solve it in a different, smarter way.
             for (int line = 0; line < lines.Length; line++)
             {
                 for (int column = 0; column < lines[line].Length; column++)
                 {
                     if (lines[line][column] == 'a')
                     {
-                        var costMatrix = KindOfDijikistra(context.Graph, GetNodeName(line, column));
-                        if(costMatrix.ContainsKey(context.Finish))
-                            lowest = Math.Min(lowest, costMatrix[context.Finish].steps);
+                        if (costMatrix.ContainsKey(GetNodeName(line, column)))
+                            lowest = Math.Min(lowest, costMatrix[GetNodeName(line, column)].steps);
                     }
                 }
             }
