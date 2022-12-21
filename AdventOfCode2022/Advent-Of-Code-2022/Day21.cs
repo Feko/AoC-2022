0c01b8cde61ce8b682bcf9e13bd3563568a9d91e
Feko <fekomedeiros@gmail.com>
2022-12-21 13:08:56 +0000

    Day 21, Part 2

--- AdventOfCode2022/Advent-Of-Code-2022/Day21.cs
+++ AdventOfCode2022/Advent-Of-Code-2022/Day21.cs
@@ -21,6 +21,7 @@ namespace TestProject1
                     "/" => BigInteger.Divide(num1, num2),
                     "-" => BigInteger.Subtract(num1, num2),
                     "+" => BigInteger.Add(num1, num2),
+                    "=" => num1,
                     _ => throw new NotImplementedException()
                 };
             }
@@ -29,9 +30,11 @@ namespace TestProject1
         public class MonkeyOperation
         {
             public string Name { get; private set; }
+            public string Operation { get; set; }
+            public MonkeyOperation Parent { get; set; }
+
             BigInteger? Value;
             string[] OtherMonkeys = new string[0];
-            public string Operation { get; set; }
             MonkeyOperation Monkey1;
             MonkeyOperation Monkey2;
 
@@ -44,20 +47,19 @@ namespace TestProject1
                     Value = long.Parse(parts[1]);
 
                 else
-                { 
+                {
                     var exp = parts[1].Split(' ');
                     Operation = exp[1];
                     OtherMonkeys = new[] { exp[0], exp[2] };
-                } 
+                }
             }
 
             public BigInteger GetValue()
             {
-                if(Value.HasValue)
+                if (Value.HasValue)
                     return Value.Value;
 
-                Value = BigIntExpressionEvaluator.Exec(Operation, Monkey1.GetValue(), Monkey2.GetValue());
-                return Value.Value;
+                return BigIntExpressionEvaluator.Exec(Operation, Monkey1.GetValue(), Monkey2.GetValue());
             }
 
             public void AddReferences(Dictionary<string, MonkeyOperation> allMonkeys)
@@ -67,6 +69,49 @@ namespace TestProject1
 
                 Monkey1 = allMonkeys[OtherMonkeys[0]];
                 Monkey2 = allMonkeys[OtherMonkeys[1]];
+                Monkey1.Parent = this;
+                Monkey2.Parent = this;
+            }
+
+            public void Equalize(List<string> sequence)
+            {
+                //This method is only called at the root node
+                var valueFrom = sequence.First() == OtherMonkeys.First() ? Monkey2 : Monkey1;
+                Operation = "=";
+                Equalize(sequence, valueFrom.GetValue());
+            }
+
+            public void Equalize(List<string> sequence, BigInteger expectedValue)
+            {
+                //Is this the final node?
+                if (!sequence.Any())
+                {
+                    Value = expectedValue;
+                    return;
+                }
+
+                (var nodeToNormalize, var valueFromNode, bool normalizingRight) = (Monkey2, Monkey1, true);
+                if (sequence.First() == OtherMonkeys.First())
+                    (nodeToNormalize, valueFromNode, normalizingRight) = (Monkey1, Monkey2, false);
+
+                var otherHandValue = valueFromNode.GetValue();
+                var valueNeeded = GetValueNeeded(expectedValue, normalizingRight, otherHandValue);
+
+                sequence.RemoveAt(0);
+                nodeToNormalize.Equalize(sequence, valueNeeded);
+            }
+
+            private BigInteger GetValueNeeded(BigInteger expectedValue, bool normalizingRight, BigInteger otherHandValue)
+            {
+                return Operation switch
+                {
+                    "+" => expectedValue - otherHandValue,
+                    "-" => normalizingRight ? otherHandValue - expectedValue : expectedValue + otherHandValue,
+                    "*" => expectedValue / otherHandValue,
+                    "/" => normalizingRight ? otherHandValue / expectedValue : expectedValue * otherHandValue,
+                    "=" => expectedValue,
+                    _ => throw new Exception("This... is not supposed to happen")
+                };
             }
         }
 
@@ -74,7 +119,7 @@ namespace TestProject1
         public void Day06_Part1()
         {
             //long expected = 152; var lines = File.ReadAllLines("Inputs/day21_sample.txt");
-            long expected = 152; var lines = File.ReadAllLines("Inputs/day21.txt");
+            long expected = 169525884255464; var lines = File.ReadAllLines("Inputs/day21.txt");
 
             var allMonkeys = lines.Select(line => new MonkeyOperation(line)).ToDictionary(k => k.Name, v => v);
 
@@ -82,6 +127,39 @@ namespace TestProject1
                 v.AddReferences(allMonkeys);
 
             Assert.Equal(expected, allMonkeys["root"].GetValue());
+        }
+
+        [Fact]
+        public void Day06_Part2()
+        {
+            //long expected = 301; var lines = File.ReadAllLines("Inputs/day21_sample.txt");
+            long expected = 3247317268284; var lines = File.ReadAllLines("Inputs/day21.txt");
+
+            var allMonkeys = lines.Select(line => new MonkeyOperation(line)).ToDictionary(k => k.Name, v => v);
+
+            foreach (var (k, v) in allMonkeys)
+                v.AddReferences(allMonkeys);
+
+            var theMonkeyIAm = allMonkeys["humn"];
+            var normalizationSequence = GetNormalizationSequence(theMonkeyIAm);
+
+            allMonkeys["root"].Equalize(normalizationSequence);
+
+            Assert.Equal(expected, theMonkeyIAm.GetValue());
+        }
+
+        private static List<string> GetNormalizationSequence(MonkeyOperation theMonkeyIAm)
+        {
+            List<string> normalizationSequence = new();
+
+            MonkeyOperation current = theMonkeyIAm;
+            while (current.Parent != null)
+            {
+                normalizationSequence.Add(current.Name);
+                current = current.Parent;
+            }
+            normalizationSequence.Reverse();
+            return normalizationSequence;
         }
     }
 }
