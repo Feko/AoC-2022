2ee90a7b9ab8bf006abc59321d19ef94e508cb0a
Feko <fekomedeiros@gmail.com>
2022-12-22 15:59:53 +0000

    Day 22, idea of having rules. Not yet in place.

--- AdventOfCode2022/Advent-Of-Code-2022/Day22.cs
+++ AdventOfCode2022/Advent-Of-Code-2022/Day22.cs
@@ -19,6 +19,43 @@ namespace TestProject1
             Up = 3,
         }
 
+        interface WrapStrategy
+        {
+            (int line, int column, FacingDirection direction) Wrap((int line, int column) pos, FacingDirection direction);
+            bool IsOutbounds((int line, int column) pos);
+        }
+
+        class Part1Wrap : WrapStrategy
+        {
+            public char[,] Grid { get; }
+
+            public Part1Wrap(char[,] grid)
+            {
+                Grid = grid;
+            }
+
+            public bool IsOutbounds((int line, int column) pos)
+            {
+                return pos.line < 0 || pos.column < 0 || pos.line >= Grid.GetLength(0) || pos.column >= Grid.GetLength(1);
+            }
+
+            public (int line, int column, FacingDirection direction) Wrap((int line, int column) pos, FacingDirection direction)
+            {
+                (int line, int column) newPos = (pos.line, pos.column);
+
+                if (pos.line < 0)
+                    newPos.line = Grid.GetLength(0) - 1;
+                if (pos.line >= Grid.GetLength(0))
+                    newPos.line = 0;
+                if (pos.column < 0)
+                    newPos.column = Grid.GetLength(1) - 1;
+                if (pos.column >= Grid.GetLength(1))
+                    newPos.column = 0;
+
+                return (newPos.line, newPos.column, direction);
+            }
+        }
+
         const char EMPTY = ' ';
         const char NAVIGABLE = '.';
         const char WALL = '#';
@@ -42,6 +79,59 @@ namespace TestProject1
             Print();
             Console.WriteLine($"\n\nFinished on line {CurrentLine + 1}, column {CurrentColumn + 1} and facing  {CurrentFacing}.");
             Console.WriteLine($"Score: {(1000 * (CurrentLine + 1)) + (4 * (CurrentColumn + 1)) + ((int)CurrentFacing)}");
+        }
+
+        [Fact]
+        public void Day22_Part2()
+        {
+            var text = File.ReadAllText("Inputs/day22_sample.txt");
+            //var text = File.ReadAllText("Inputs/day22.txt");
+            var parts = text.Split("\n\n");
+            var gridLines = parts[0].Split("\n");
+            var movements = parts[1];
+
+            CreateGrid(gridLines);
+            PerformMovements(movements);
+            Print();
+            Console.WriteLine($"\n\nFinished on line {CurrentLine + 1}, column {CurrentColumn + 1} and facing  {CurrentFacing}.");
+            Console.WriteLine($"Score: {(1000 * (CurrentLine + 1)) + (4 * (CurrentColumn + 1)) + ((int)CurrentFacing)}");
+        }
+
+        private void CreateWrapRules(int pieceSize = 4)
+        {
+            (int line, int column) pos = (1,5);
+            var result = pos switch 
+            {
+                //1 intersects 2
+                { line: -1}                => (4, 3 - (pos.column % pieceSize), FacingDirection.Down), //1 intersects 2, going from 1 to 2
+                { line: 3, column: <= 3}   => (0, 11 - pos.column, FacingDirection.Down), //1 intersects 2, going from 2 to 1
+
+                //5 intersects 2
+                { line: 12,column: <= 11 } => (7, 3 - (pos.column % pieceSize), FacingDirection.Up), //5 intersects 2, going from 5 to 2
+                { line: 8, column: <= 3 }  => (11, 11 - pos.column, FacingDirection.Up), //5 intersects 2, going from 2 to 5
+
+                //2 intersects 6
+                { column: < 0 }            => (11, 15 - (pos.line % pieceSize), FacingDirection.Up), //2 intersects 6, going from 2 to 6
+                { line: 12, column: > 11 } => (7 - (pos.column % pieceSize), 0, FacingDirection.Right), //2 intersects 6, going from 6 to 2
+
+                //6 intersects 1
+                { column: 16 } => (3 - (pos.line % pieceSize), 11, FacingDirection.Left), //6 intersects 1, going from 6 to 1
+                { line: <= 3, column: 12 } => (11 - (pos.line % pieceSize), 15, FacingDirection.Left), //6 intersects 1, going from 1 to 6
+
+                //5 intersects 3
+                { line: >= 8, column: 7 }  => (7, 7 - (pos.line % pieceSize), FacingDirection.Up), //5 intersects 3, going from 5 to 3
+                { line: 8, column: >= 4}   => (11 - (pos.column % pieceSize), 8, FacingDirection.Right), //5 intersects 3, going from 3 to 5
+
+                //3 intersects 1
+                { line: 3, column: >= 4 }  => (pos.column % pieceSize, 8, FacingDirection.Right), //3 intersects 1, going from 3 to 1
+                { line: <= 3, column: 7 }  => (4, 4 + pos.line, FacingDirection.Down), //3 intersects 1, going from 1 to 3
+
+                //4 intersects 6
+                { column: 12 }             => (8, 15 - (pos.line % pieceSize), FacingDirection.Down), //4 intersects 6, going from 4 to 6
+                { line: 7, column: >= 12 } => (7 - (pos.column % pieceSize), 11, FacingDirection.Left), //4 intersects 6, going from 6 to 4
+
+                _ => throw new NotImplementedException()
+            };
         }
 
         private void Turn(char direction)
